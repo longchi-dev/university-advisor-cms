88d5f4750c2a6c454c7fd2528cca81921187fbfd
longchi <chilong03082003@gmail.com>
2026-04-13 10:44:34 +0700

    update

--- app/Contracts/Repositories/IThemeRepository.php
+++ app/Contracts/Repositories/IThemeRepository.php
@@ -3,8 +3,11 @@
 namespace App\Contracts\Repositories;
 
 use App\Models\Theme;
+use Illuminate\Database\Eloquent\Collection;
 
 interface IThemeRepository
 {
-    public function findById(string $id): ?Theme;
+    public function findById(int $id): ?Theme;
+    public function getAll(): Collection;
+    public function save(Theme $theme): Theme;
 }
--- app/Repositories/ThemeRepository.php
+++ app/Repositories/ThemeRepository.php
@@ -4,11 +4,23 @@ namespace App\Repositories;
 
 use App\Contracts\Repositories\IThemeRepository;
 use App\Models\Theme;
+use Illuminate\Database\Eloquent\Collection;
 
 class ThemeRepository implements IThemeRepository
 {
-    public function findById(string $id): ?Theme
+    public function findById(int $id): ?Theme
     {
         return Theme::query()->find($id);
+    }
+
+    public function getAll(): Collection
+    {
+        return Theme::query()->orderBy('order')->get();
+    }
+
+    public function save(Theme $theme): Theme
+    {
+        $theme->save();
+        return $theme;
     }
 }
